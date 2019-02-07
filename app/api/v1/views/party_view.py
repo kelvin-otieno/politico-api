3579fc6b32bc44cb8d7d340dd334d8df4d7880c4
kelvin-otieno <kelvinotieno06@gmail.com>
2019-02-07 09:24:41 +0300

    [finishes #163687770] api enables user to get individual party by id

--- app/api/v1/views/party_view.py
+++ app/api/v1/views/party_view.py
@@ -9,3 +9,12 @@ def parties_all():
     """function to retrieve all political parties"""
     party = PoliticalParty()
     return jsonify(party.get_parties())
+
+
+@bpparty.route('/<int:id>', methods=['GET'])
+def parties_id(id):
+    """function to retrieve a specific political party"""
+
+    parties = PoliticalParty()
+
+    return jsonify(parties.get_party(id))
