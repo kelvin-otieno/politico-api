2e4c5aa51756149fc69f8b8275d5702b7eb23ab8
kelvin-otieno <kelvinotieno06@gmail.com>
2019-02-07 10:15:20 +0300

    [finishes #163687720] create a new political office in the system

---
+++ app/api/v1/views/office_view.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, request, jsonify
+from app.api.v1.models.office_model import PoliticalOffice
+
+bpoffice = Blueprint('office', __name__)
+
+
+@bpoffice.route('/', methods=['POST'])
+def create_office():
+    """ Creating a political party"""
+    name = request.json['name']
+    level = request.json['level']
+
+    office = PoliticalOffice()
+    office.name = name
+    office.level = level
+
+    # POLITICAL_PARTIES.append(party)
+    return jsonify(office.create_office())
+    # return make_response(jsonify({"status": "OK", "message": "I am {}".format(full_name)}))
+    # msg = "the name is " + full_name
+    # return msg
