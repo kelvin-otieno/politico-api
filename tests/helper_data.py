3bae811721d05bc6835e031ea0c8da37792c6412
kelvin-otieno <kelvinotieno06@gmail.com>
2019-02-17 11:17:52 +0300

    push helper data and helper methods

--- tests/helper_data.py
+++ tests/helper_data.py
@@ -1,16 +1,44 @@
-"""helper data is data which will be used frequently for testing"""
+"""payloads which will be used frequently for testing"""
 OFFICE_DATA = {
-    'id': 1,
-    'type': 'Local Government',
+    'office_type': 'Local Government',
     'name': 'MCA'
 }
 
 PARTY_DATA = {
-    'id': 1,
     'name': 'ANC',
     'hqAddress': 'Bungoma',
     'logoUrl': 'amani.png'
 }
+
+
 PARTY_EDITED_DATA = {
     'name': 'WIPER'
+}
+
+VOTING_DATA = {
+
+    "voter_id": 1,
+    "office_id": 1,
+    "candidate_id": 1
+}
+USER_DATA = {
+
+    "firstname": "kelvin",
+    "lastname": "otieno",
+                "othername": "okoth",
+                "email": "kelvinotieno06@gmail.com",
+                "phoneNumber": "0712561541",
+                "passportUrl": "image.png",
+                "isAdmin": "True",
+                "password": "Iamnumber1"
+}
+
+LOGIN_DATA = {
+    "email": "kelvinotieno06@gmail.com",
+    "password": "Iamnumber1"
+}
+
+CANDIDATE_DATA = {
+    "office_id": 1,
+    "user_id": 1
 }
--- tests/helper_methods.py
+++ tests/helper_methods.py
@@ -12,3 +12,31 @@ def create_office(self, data):
     """endpoint to create new party"""
     return self.client.post(
         path='/api/v1/offices/', data=json.dumps(data), content_type='application/json')
+
+
+def create_office_v2(self, data):
+    """endpoint to create new party"""
+    return self.client.post(
+        path='/api/v2/offices/', data=json.dumps(data), content_type='application/json')
+
+
+def create_party_v2(self, data):
+    """endpoint to create new party"""
+    return self.client.post(
+        path='/api/v2/parties/', data=json.dumps(data), content_type='application/json')
+
+
+def create_user(self, data):
+    """endpoint to create new party"""
+    return self.client.post(
+        path='/api/v2/auth/signup/', data=json.dumps(data), content_type='application/json')
+
+def create_candidate(self, data):
+    """endpoint to create new party"""
+    return self.client.post(
+        path='/api/v2/office/1/register/', data=json.dumps(data), content_type='application/json')
+
+def create_vote(self, data):
+    """endpoint to create new party"""
+    return self.client.post(
+        path='/api/v2/votes/', data=json.dumps(data), content_type='application/json')
